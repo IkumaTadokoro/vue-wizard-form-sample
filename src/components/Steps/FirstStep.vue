7f595725db8199890ffa2149551d50248adbc0c6
IkumaTadokoro <tadokorodev@gmail.com>
2021-11-15 22:04:07 +0900

    FirstStepの質問を変更

--- src/components/Steps/FirstStep.vue
+++ src/components/Steps/FirstStep.vue
@@ -1,12 +1,11 @@
 <template>
   <form @input="submit">
-    <h2>Name Info</h2>
     <div class="form-group">
-      <label for="firstName">First Name</label>
+      <label for="firstName">姓</label>
       <input type="text" class="form-control" v-model="firstName" placeholder="Enter First Name">
     </div>
     <div class="form-group">
-      <label for="lastName">Last Name</label>
+      <label for="lastName">名</label>
       <input type="text" class="form-control" v-model="lastName" placeholder="Enter Last Name">
     </div>
   </form>
