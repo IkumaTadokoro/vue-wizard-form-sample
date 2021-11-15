ae13fb97b1d45efcc97a0fdabc762ba38b36a6f5
IkumaTadokoro <tadokorodev@gmail.com>
2021-11-15 21:41:42 +0900

    FirstComponentを作成

---
+++ src/components/FirstStep.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Step: {{ stepNumber }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'FirstStep',
+  data() {
+    return {
+      stepNumber: 1,
+      form: {
+        firstName: null,
+        lastName: null,
+        Email: null,
+        tel: null,
+        birthday: null
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
